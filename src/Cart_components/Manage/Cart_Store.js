cb3806dbfc0b41e6f8550907271979093a04472c
Nicolas Liu <st880517@gmail.com>
2021-10-31 16:49:05 +0800

    20211031

--- src/Cart_components/Manage/Cart_Store.js
+++ src/Cart_components/Manage/Cart_Store.js
@@ -1,50 +1,84 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import $, { data } from 'jquery'
+import $ from 'jquery'
+// import { MapContainer } from 'react-leaflet'
+
 // import Iframe from 'react-iframe'
 import { FaChevronDown } from 'react-icons/fa'
 
 function Cart_Store(props) {
+  const options = props.pathOptions ?? {}
+
   let { StoreInfo, setStoreInfo, CityArr, setCityArr } = props
   let [StoreCity, setStoreCity] = useState([])
-  let Address = []
-  console.log(CityArr)
+  let [GetAddress, setGetAddress] = useState([])
+  let [GetStoreInfo, setGetStoreInfo] = useState([])
+  let [test, settest] = useState(0)
+  let SearchInfo = 'SearchRoad'
+  // console.log(CityArr)
+  // console.log('店名資訊', StoreCity)
+  // console.log('路段資訊', GetAddress)
+
   useEffect(() => {
     if (StoreCity[0] && StoreCity[1]) {
       DataAxios()
     }
-  }, [StoreCity])
+  }, [StoreCity, test])
+
+  useEffect(() => {
+    console.log('有抓到 GetAddress', GetAddress)
+  }, [])
 
   async function DataAxios() {
-    console.log('City', StoreCity[0])
-    console.log('District', StoreCity[1])
+    if (StoreCity[2]) {
+      SearchInfo = 'SearchStore'
+    }
+    // console.log('City', StoreCity[0])
+    // console.log('District', StoreCity[1])
+    // console.log('更改的Data', SearchInfo)
+    console.log('SearchINfo狀態', SearchInfo)
+
     let r = await axios.post('http://localhost:3001/cart/store', {
-      commandid: 'SearchStore',
+      commandid: SearchInfo,
       city: StoreCity[0],
       town: StoreCity[1],
-      // roadname: '臨沂街',
+      roadname: StoreCity[2],
     })
     if (r.status === 200) {
-      console.log('DataX', r.data)
-      const jqAddress = $(r.data)
-      const jqPOIName = $(r.data)
-      jqPOIName.find('GeoPosition POIName').each((i, el) => {
-        console.log('店名', el.innerText)
-        Address[i] = { Name: el.innerText + '門市', Address: '' }
+      // console.log('DataX', r.data)
+      const jqRoadName = $(r.data)
+      jqRoadName.find('RoadName rd_name_1').each((i, el) => {
+        // console.log('路名', el.innerText)
+        GetAddress[i] = { RoadName: el.innerText }
       })
-      jqAddress.find('GeoPosition Address').each((i, el) => {
-        console.log('地址', el)
-        Address[i].Address = el.innerText
+      jqRoadName.find('RoadName section_1').each((i, el) => {
+        // console.log('地址', el)
+        GetAddress[i].RoadName += el.innerText
       })
 
-      console.log('店名陣列', Address)
+      const jqStoreInfo = $(r.data)
+      jqStoreInfo.find('GeoPosition POIName').each((i, el) => {
+        console.log('路名', el.innerText)
+        GetStoreInfo[i] = { POIName: el.innerText, X: '', Y: '' }
+      })
+      jqStoreInfo.find('GeoPosition X').each((i, el) => {
+        // console.log('路名', el.innerText)
+        GetStoreInfo[i].X = parseInt(el.innerText) * 0.000001
+      })
+      jqStoreInfo.find('GeoPosition Y').each((i, el) => {
+        // console.log('路名', el.innerText)
+        GetStoreInfo[i].Y = parseInt(el.innerText) * 0.000001
+      })
+      SearchInfo = 'SearchRoad'
+      console.log('路段', GetAddress)
+      console.log('店鋪資訊', GetStoreInfo)
     }
   }
 
   function UpdateInfo(value, index) {
     let NewStoreCIty = [...StoreCity]
     NewStoreCIty[index] = value
-    console.log('123', NewStoreCIty)
+    // console.log('123', NewStoreCIty)
     setStoreCity(NewStoreCIty)
   }
 
@@ -60,7 +94,7 @@ function Cart_Store(props) {
             <h4>已選擇門市名稱:鑫復門市</h4>
           </div>
           <div className="store_711_address col-lg-5 col-10">
-            <h4>門市地址:台北市大安區信義路三段178號1樓</h4>
+            <h4>門市地址:{StoreCity[2]}</h4>
           </div>
         </div>
         <form className="storeform" action="">
@@ -72,7 +106,7 @@ function Cart_Store(props) {
               name="city"
               id="city"
               onChange={(e) => {
-                console.log('選到的A', e.target.value)
+                // console.log('選到的A', e.target.value)
                 UpdateInfo(e.target.value, 0)
               }}
             >
@@ -90,10 +124,21 @@ function Cart_Store(props) {
               name="districts"
               id="districts"
               onChange={(e) => {
-                console.log('選到的B', e.target.value)
+                // console.log('選到的B', e.target.value)
+                if (StoreCity[2]) {
+                  StoreCity.splice(2, 1, '')
+                  console.log('第二層資料刪除', StoreCity)
+                }
                 UpdateInfo(e.target.value, 1)
+                GetAddress = []
+                setGetAddress(GetAddress)
+
+                // SearchInfo = 'SearchStore'
+                // console.log('更改的資料', SearchInfo)
+                // DataAxios()
               }}
             >
+              {console.log('第二層', StoreCity)}
               {CityArr.map((v, i) => {
                 if (v.City === StoreCity[0]) {
                   return v.districts.map((a) => {
@@ -108,12 +153,27 @@ function Cart_Store(props) {
             <span>*</span>請選擇街道名稱
           </label>
           <div className="dropdown">
-            <select name="city" id="city">
-              <option value="1">台北市</option>
-              <option value="2">新北市</option>
+            <select
+              name="roadname"
+              id="roadname"
+              onClick={(e) => {
+                settest(test + 1)
+                console.log('測試數量', test)
+              }}
+              onChange={(e) => {
+                GetAddress = []
+
+                console.log('選到的C', e.target.value)
+                UpdateInfo(e.target.value, 2)
+              }}
+            >
+              {GetAddress.map((v, i) => {
+                return <option value={v.RoadName}>{v.RoadName}</option>
+              })}
             </select>
             <FaChevronDown className="ChevronDown position-absolute" />
           </div>
+
           {/* <Iframe
             className="col-12 my-5"
             url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1807.5132236306938!2d121.54191407519947!3d25.033176522941467!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442abd49963984b%3A0x57f2f5ed99bc208!2zNy1FTEVWRU4g6ZGr5b6p6ZaA5biC!5e0!3m2!1szh-TW!2stw!4v1634475462113!5m2!1szh-TW!2stw"
